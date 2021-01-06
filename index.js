118d2de937159b5902537eb4722c3e238a4170f8
bowen <447099673@qq.com>
2021-01-06 22:25:21 +1100

    Finished overview of Context

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const Koa = require('koa');
 const app = new Koa();
 
+// add a date method to the context
+app.context.date = Date();
+app.context.userData = {
+  'first': 'Manny',
+  'last': 'Henri',
+}
+
+// response
 app.use(ctx => {
-  ctx.body = 'Hello Manny';
+
+  if (ctx.userData) {
+    return ctx.response.body = ctx.userData;
+  } else {
+    return ctx.throw(400, 'Data required!');
+  }
 });
 
 app.listen(3000);
