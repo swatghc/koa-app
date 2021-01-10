2d6f40564b6586eeb23c0a1678935b112544b275
bowen <447099673@qq.com>
2021-01-10 21:55:21 +1100

    Cascading Middleware

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const Koa = require('koa');
 const app = new Koa();
 
-// add a date method to the context
-app.context.date = Date();
 app.context.userData = {
   'first': 'Manny',
   'last': 'Henri',
 }
 
-// response
-app.use(ctx => {
 
-  if (ctx.userData) {
-    return ctx.response.body = ctx.userData;
-  } else {
-    return ctx.throw(400, 'Data required!');
-  }
+// log
+app.use(async (ctx, next) => {
+  await next();
+  const responseTime = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${responseTime}`);
+})
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const millisecond = Date.now() - start;
+  ctx.set('X-Response-Time', `${millisecond}ms`);
+})
+
+// response
+app.use(async (ctx) => {
+  ctx.response.body = ctx.userData;
 });
 
 app.listen(3000);
